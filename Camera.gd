0182b50a7252d6be0a7df77a577c456713acde05
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-18 23:28:03 +0900

    Modify camera

--- Camera.gd
+++ Camera.gd
@@ -13,4 +13,18 @@ func _ready():
 		set_process(true)
 
 func _process(delta):
-	self.set_position(player.get_position().linear_interpolate(target.position, 0.5))
+	var dist = player.get_position().distance_to(target.get_position())
+	var lim = 600
+	
+	var zoom_val = max(0.5, dist/lim)
+	self.zoom = Vector2(zoom_val, zoom_val)
+
+	var camera_pose = player.get_position().linear_interpolate(target.position, 0.5)
+
+	var direction = camera_pose.direction_to(player.get_position()).x
+	if direction < 0:
+		direction = -1
+	else:
+		direction = 1
+		
+	self.set_position(camera_pose+Vector2(direction*50, 0))
