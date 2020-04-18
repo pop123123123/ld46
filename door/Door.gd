fab7bd25ec71386941c970906342b72e95f71578
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-19 01:20:03 +0900

    Add whistle action

--- door/Door.gd
+++ door/Door.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+enum Tool{
+	WHISTLE,
+	UMBRELLA,
+	GUN,
+	CROWBAR
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +15,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
 func use_tool(tool_index, player_pos):
-	if self.position.x-player_pos.x < 10:
-		print("RIP LA PORTE")
-		hide()
-	else:
-		print("Whoup whoup ca wiffe !")
+	if tool_index == Tool.CROWBAR:
+		if self.position.x-player_pos.x < 10:
+			print("RIP LA PORTE")
+			hide()
+		else:
+			print("Whoup whoup ca wiffe !")
