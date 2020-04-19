948b204a449df38b52392e700162451ca05813ad
Louis MOUHAT <louis.mouhat@gmail.com>
2020-04-19 12:21:41 +0900

    add loops to drones

--- drone/drone.gd
+++ drone/drone.gd
@@ -3,6 +3,11 @@ extends RigidBody2D
 const UP = Vector2(0, -1)
 
 export var speed = 1.0
+export var loop = false
+export var loop_distance = 40
+
+var pos_start = null
+var pos_end = null
 
 var width = null
 var up = null
@@ -12,6 +17,9 @@ var gravity = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pos_start = self.position.x
+	pos_end = pos_start + sign(speed) * loop_distance
+	
 	var mi = INF
 	var ma = -INF
 	for p in $Shape.get_polygon():
@@ -29,7 +37,7 @@ func _ready():
 
 #func _process(delta):
 #	pass
-var i = 0
+var turning = true
 
 func _physics_process(delta):
 	var local_left_propeller = self.get_transform().basis_xform(left_propeller.normalized()) * left_propeller.length()
@@ -39,16 +47,24 @@ func _physics_process(delta):
 
 	var c = 4
 	var acc = 6
-	var a = f / 2 * (1 if speed < 0 else -1)
-	var frames = 7 * (speed if speed > 0 else -speed)
-	if i < ceil(frames):
-		var left = 1 if i < frames else frames-floor(frames)
+	var a = - f / 2 * sign(speed)
+	if turning and abs(self.linear_velocity.x) < abs(speed) or sign(speed) != sign(self.linear_velocity.x):
+		var left = min(1, abs(speed - self.linear_velocity.x))
 		self.apply_impulse(local_left_propeller, -a*left/c)
 		self.apply_impulse(local_right_propeller, a*left/c)
-		self.apply_central_impulse(Vector2(f.x*acc, 0))
-		i += 1
-	elif i == ceil(frames):
-		self.apply_central_impulse(Vector2(f.x*acc, 0))
-		i += 1
+		if sign(f.x) == sign(speed):
+			self.apply_central_impulse(Vector2(f.x*acc, 0))
+	elif turning:
+		self.set_linear_velocity(Vector2(speed, self.get_linear_velocity().y))
+		self.set_angular_velocity(0)
+		turning = false
 	else:
 		self.set_angular_velocity(0)
+	
+	if loop:
+		if abs(self.position.x - pos_start) > abs(loop_distance) and sign(speed) != abs(self.position.x - pos_start) and not turning:
+			speed *= -1
+			var start = pos_start
+			pos_start = pos_end
+			pos_end = start
+			turning = true
