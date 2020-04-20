1991fdcbbc95dbd7d7cef845373db99fc4823910
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-20 19:43:18 +0900

    Add missing reload signal

--- Stage.gd
+++ Stage.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-# Todo: var level_scenes = [preload("res://level0.tscn"), ...]
-var level_scenes = []
+var level_scenes = [preload("res://tuto/Tuto.tscn")]
 var current_level_index = 0
 
 var current_level
@@ -19,7 +18,12 @@ func _end_of_level():
 	else:
 		_load_level(current_level_index)
 	
+func _reload_level():
+	self.remove_child(current_level)
+	_load_level(current_level_index)
+	
 func _load_level(index):
 	current_level = level_scenes[index].instance()
 	add_child(current_level)
 	current_level.connect("end_of_level", self, "_end_of_level")
+	current_level.connect("reload_level", self, "_reload_level")
