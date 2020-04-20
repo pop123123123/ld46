1771cebe474b7beb50374c9a2f1ea2335ea354de
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-20 11:39:51 +0900

    Change simple camera following the player

--- Camera.gd
+++ Camera.gd
@@ -16,8 +16,10 @@ func _process(_delta):
 	#var dist = player.get_position().distance_to(target.get_position())
 	#var lim = 600
 	
-	var la_distance = (player.get_position()-target.get_position())*1.2
-	var zoom_val = max(1, sqrt(1*la_distance.length()/300))
+	#var la_distance = (player.get_position()-target.get_position())*1.2
+	#var zoom_val = max(1, sqrt(1*la_distance.length()/300))
 	
-	self.zoom = Vector2(zoom_val, zoom_val)
-	self.set_position(target.get_position()+la_distance/2)
+	#self.zoom = Vector2(zoom_val, zoom_val)
+	#self.set_position(target.get_position()+la_distance/2)
+	
+	self.position = player.get_position()
