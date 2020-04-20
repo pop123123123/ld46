0c55c0b14a2fbc9dc168dcb45e14dea51c7a8de6
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-20 12:03:26 +0900

    Fix target multiple deaths

--- target/Target.gd
+++ target/Target.gd
@@ -10,7 +10,8 @@ enum Tool{
 
 export var force = 1 * 60
 var gravity = force * 1000
-var moving = true
+
+var is_alive = true
 
 export var acid_resistance = 3
 var lifepoints
@@ -21,22 +22,21 @@ func _ready():
 	lifepoints = acid_resistance
 
 func _process(_delta):
-	if (prevPosition - self.position).length() > 1.5:
-		($damage as AudioStreamPlayer2D).play()
-	if moving:
+	if is_alive:
+		if (prevPosition - self.position).length() > 1.5:
+			($damage as AudioStreamPlayer2D).play()
+			
 		if not ($walk as AudioStreamPlayer2D).is_playing():
 			($walk as AudioStreamPlayer2D).play()		
 		($AnimatedSprite as AnimatedSprite).play("running")
-	prevPosition = self.position
+		prevPosition = self.position
 
 func _physics_process(delta):
-	if moving:
+	if is_alive:
 		if move_and_collide(Vector2(0, 5), false, false, true):
 			self.move_and_slide_with_snap(Vector2(force*delta, 0), Vector2(0, 20), Vector2(0, 1), true, 4, PI)
 		else:
 			self.move_and_slide_with_snap(Vector2(0, gravity*delta), Vector2(0, 20), Vector2(0, 1), true, 4, PI)
-	else:
-		pass
 
 func use_tool(tool_index, _player_pos):
 	if tool_index == Tool.WHISTLE:
@@ -51,11 +51,12 @@ func _on_HurtboxArea_body_entered(_body):
 		_kill_target()
 
 func _kill_target():
-	moving = false
-	($death as AudioStreamPlayer2D).play()
-	($AnimatedSprite as AnimatedSprite).play("death")
-	#hide()
-	emit_signal("gameover")
+	if is_alive:
+		is_alive = false
+		($death as AudioStreamPlayer2D).play()
+		($AnimatedSprite as AnimatedSprite).play("death")
+		
+		emit_signal("gameover")
 
 func _on_LifebarTimer_timeout():
 	lifepoints = acid_resistance
