812d60864534f4db2048376f3450da816fb395f5
Louis MOUHAT <louis.mouhat@gmail.com>
2020-04-20 23:56:02 +0900

    fix tree bug on gameover

--- gameover/GameOverScreen.gd
+++ gameover/GameOverScreen.gd
@@ -1,19 +1,10 @@
 extends CanvasLayer
 signal retry
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var me
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	me = get_tree()
 
 # Set GameOver screen size to window size
 func update_to_window_size():
@@ -27,9 +18,8 @@ func launch_game_over():
 	($loose as AudioStreamPlayer).play()
 	update_to_window_size()
 	# Wait a second before showing GameOver screen
-	yield(get_tree().create_timer(1), "timeout")
+	yield(me.create_timer(1), "timeout")
 	var gameOver = get_node("GameOverNode")
-	var moi = get_tree()
 	while gameOver.modulate.a < 255:
 		gameOver.modulate.a  += 1
-		yield(moi,"idle_frame")
+		yield(me,"idle_frame")
