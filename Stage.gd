ceae8b6208905987e5b8b0af7b0e3ab4d2966a57
Nicolas BUSSER <nicolas.busser@grenoble-inp.org>
2020-04-20 18:47:09 +0900

    Finish dynamic level instanciation

--- Stage.gd
+++ Stage.gd
@@ -1,1 +1,25 @@
 extends Node2D
+
+# Todo: var level_scenes = [preload("res://level0.tscn"), ...]
+var level_scenes = []
+var current_level_index = 0
+
+var current_level
+
+func _ready():
+	_load_level(current_level_index)
+
+func _end_of_level():
+	self.remove_child(current_level)
+	current_level_index += 1
+	
+	if current_level_index == level_scenes.size():
+		#TODO: credits screen ?
+		pass
+	else:
+		_load_level(current_level_index)
+	
+func _load_level(index):
+	current_level = level_scenes[index].instance()
+	add_child(current_level)
+	current_level.connect("end_of_level", self, "_end_of_level")
